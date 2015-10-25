22c0fd2606e3c2acba7407cff6136676ac0eca41
mib008 <mib008@126.com>
2015-10-25 17:59:21 +0800

    VS2013 Support

---
+++ AngularJsDemos/server.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+var port = process.env.port || 1337;
+
+var child_process = require("child_process"),
+    path = require("path");
+
+var spawn = child_process.spawn;
+
+var basePath = __dirname;
+
+var cmd = path.join(basePath, "./node_modules/.bin/http-server");
+
+console.log("CMD: " + cmd);
+
+var app = child_process.exec(cmd, ['-a','localhost','-p', port, '-c-1'], {}, function (e, stdout, stderr) {
+    if (e) console.log(JSON.stringify(e));
+
+    console.log(stdout);
+    console.log(stderr); 
+});
+
+app.stdout.resume();
+
+app.stdout.on('data', function(data) {
+    console.log(data);
+});
